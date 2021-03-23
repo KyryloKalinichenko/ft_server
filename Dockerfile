8dbffda7897e63c8db5d8cf50e41f0bd23d3c530
KyryloKalinichenko <kirillstomper@gmail.com>
2021-03-23 12:15:50 +0100

    faster

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM debian:buster
 RUN apt-get update -y && apt-get upgrade -y && apt-get install nginx -y && apt-get -y install mariadb-server && apt-get -y install wget
 RUN apt-get -y install php7.3 php-mysql php-fpm php-pdo php-gd php-cli php-mbstring
 
+EXPOSE 80
+EXPOSE 443
 COPY srcs /
 
 RUN service mysql start
@@ -10,4 +12,23 @@ RUN service mysql start
 RUN chown -R www-data /var/www/*
 RUN chmod -R 755 /var/www/*
 
+RUN mkdir /var/www/here && touch /var/www/here/info.php
+RUN echo "<?php phpinfo(); ?>" >> /var/www/here/info.php
+
+RUN mv ./here /etc/nginx/sites-available/here
+RUN ln -s /etc/nginx/sites-available/here /etc/nginx/sites-enabled/here
+RUN rm -rf /etc/nginx/sites-enabled/default 
+
+RUN mkdir /var/www/here/phpmyadmin
+RUN wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
+RUN tar -xvf phpMyAdmin-4.9.0.1-all-languages.tar.gz --strip-components 1 -C /var/www/here/phpmyadmin
+RUN cd && cp /config.inc.php /var/www/here/phpmyadmin/
+
+
+
+RUN cd /var/www/here && wget https://wordpress.org/latest.tar.gz
+RUN cd /var/www/here && tar -xvzf latest.tar.gz && rm -rf latest.tar.gz
+RUN cd && cp /wp-config.php /var/www/here/wordpress/
+
 ENTRYPOINT [ "/bin/bash", "-C", "run.sh"]
+#RUN bash
--- srcs/run.sh
+++ srcs/run.sh
@@ -4,16 +4,16 @@
 #######################################
 service mysql start
 #######################################
-mkdir /var/www/here && touch /var/www/here/info.php
-echo "<?php phpinfo(); ?>" >> /var/www/here/info.php
+#mkdir /var/www/here && touch /var/www/here/info.php
+#echo "<?php phpinfo(); ?>" >> /var/www/here/info.php
 
 mkdir /etc/ssl
 openssl req -x509 -nodes -days 365 -subj "/C=BE/ST=Brussels/L=Brussels/O=19School/OU=student/CN=here" -newkey rsa:2048 -keyout /etc/ssl/localhost.key -out /etc/ssl/localhost.crt
 #######################################
 
-mv ./here /etc/nginx/sites-available/here
-ln -s /etc/nginx/sites-available/here /etc/nginx/sites-enabled/here
-rm -rf /etc/nginx/sites-enabled/default 
+#mv ./here /etc/nginx/sites-available/here
+#ln -s /etc/nginx/sites-available/here /etc/nginx/sites-enabled/here
+#rm -rf /etc/nginx/sites-enabled/default 
 
 #######################################
 
@@ -24,18 +24,19 @@ echo "update mysql.user set plugin='' where user='root';"| mysql -u root --skip-
 
 #######################################
 
-mkdir /var/www/here/phpmyadmin
-wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
-tar -xvf phpMyAdmin-4.9.0.1-all-languages.tar.gz --strip-components 1 -C /var/www/here/phpmyadmin
-cd && cp /config.inc.php /var/www/here/phpmyadmin/
+#mkdir /var/www/here/phpmyadmin
+#wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
+#tar -xvf phpMyAdmin-4.9.0.1-all-languages.tar.gz --strip-components 1 -C /var/www/here/phpmyadmin
+#cd && cp /config.inc.php /var/www/here/phpmyadmin/
 
 #######################################
 
-cd /var/www/here && wget https://wordpress.org/latest.tar.gz
-cd /var/www/here && tar -xvzf latest.tar.gz && rm -rf latest.tar.gz
-cd && cp /wp-config.php /var/www/here/wordpress/
+#cd /var/www/here && wget https://wordpress.org/latest.tar.gz
+#cd /var/www/here && tar -xvzf latest.tar.gz && rm -rf latest.tar.gz
+#cd && cp /wp-config.php /var/www/here/wordpress/
 
 service nginx start
 service mysql restart
 service php7.3-fpm start
-tail -f /dev/null
+#tail -f /dev/null
+bash
